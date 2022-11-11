84738649311442c88534f25fb2bc6bb214333a0f
User <user@pc-1888.home>
2022-11-11 12:58:36 +0100

    ania

--- main.py
+++ main.py
@@ -6,14 +6,12 @@ from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
 from pyctcdecode import build_ctcdecoder
 from wavio import read
 import jiwer
-import arpa
-import random
 
 processor=Wav2Vec2Processor.from_pretrained('facebook/wav2vec2-base-10k-voxpopuli-ft-pl')
-model=Wav2Vec2ForCTC.from_pretrained('facebook/wav2vec2-base-10k-voxpopuli-ft-pl') #.to('cuda')
+model=Wav2Vec2ForCTC.from_pretrained('facebook/wav2vec2-base-10k-voxpopuli-ft-pl')
 
 files={}
-for f in Path('sanah').glob('*.wav'):
+for f in Path('wyznanie').glob('*.wav'):
     print(f)
     data=read(str(f))
     files[f.stem]=data.data.squeeze().astype('float32')
@@ -25,42 +23,26 @@ for name,d in files.items():
 
 trans={}
 for name,data in tqdm(files.items()):
-    feats=processor(data,sampling_rate=Fs,return_tensors='pt',padding=True) #.to('cuda')
-    print(name, feats.input_values)
+    feats=processor(data,sampling_rate=Fs,return_tensors='pt',padding=True)
     out=model(input_values=feats.input_values)
     predicted_ids=torch.argmax(out.logits,dim=-1)
     sent=processor.batch_decode(predicted_ids)[0]
     trans[name]=sent
 
 ref={}
-with open('sanah/text') as f:
-    for l in f:
-        tok=l.strip().split()
-        ref[tok[0]]=' '.join(tok[1:])
 
 
 h=[]
 r=[]
 
-for name in trans.keys():
+print('trans',trans)
+for idx in range(1, len(trans.keys())+1):
+    name = f'ania{idx}'
+    print('-------------------')
     print(f'>>{name}')
     print(trans[name])
-    print(ref[name])
     print('')
 
     h.append(trans[name])
-    r.append(ref[name])
 
-print(jiwer.compute_measures(r,h))
-
-digits=['zero','jeden','dwa','trzy','cztery','pięć','sześć','siedem','osiem','dziewięć']
-with open('digits.txt','w') as f:
-  for l in range(100):
-    f.write(' '.join([digits[x] for x in random.sample(range(0, 10), 10)])+'\n')
-
-
-tokens=[x[0] for x in sorted(processor.tokenizer.get_vocab().items(),key=lambda x:x[1])]
-print(tokens)
-tokens[4]=' '
-print(tokens)
 
